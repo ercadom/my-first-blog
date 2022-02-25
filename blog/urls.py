264e916ec081b0781243a1f7c749c429b610ea69
ercadom@gmail.com <ercadom@gmail.com>
2022-02-25 16:32:12 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
-# post_list という名前の ビュー をルートURLに割り当てる
+# 例）ブラウザにhttp://127.0.0.1:8000/post/5/を入力すると、
+# Djangoはpost_detailというビューを探していると理解します。
+# そしてpkが5という情報をそのビューに転送します。
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
