07f9d0cec7f32cac8830e937aa86e6b61ee3a623
ercadom@gmail.com <ercadom@gmail.com>
2022-02-28 13:58:38 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path
+from django.urls import path 
 from . import views
-
-# 例）ブラウザにhttp://127.0.0.1:8000/post/5/を入力すると、
-# Djangoはpost_detailというビューを探していると理解します。
-# そしてpkが5という情報をそのビューに転送します。
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
